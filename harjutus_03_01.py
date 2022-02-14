fix standard deviation confidence interval

harjutus_03_01.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import math
 import pandas as pd
 import scipy.stats as st
+from scipy.stats.distributions import chi2
+
 df = pd.read_csv('harjutus_01_autod.csv', delimiter=';')
 df = df.rename(columns=lambda x: x.strip())
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
@@ -21,17 +24,41 @@ mean = st.t.interval(
     alpha=0.95, df=weight.size-1,
     loc=weight.mean(), scale=weight.sem()
 )
-
+# kaaluvahemik 95% usaldusnivoo korral
+#  vahemikhinnang
+# mu traditsiooniliselt tähistatakse keskväärtust
+# need autod meie käsutuses valim
+# weight.mean() punkthinnag
+# mean[0] ja mean[1] usalduspiirid
 print("      mu = %.2f [%.2f, %.2f]" %
       (weight.mean(), mean[0], mean[1])
       )
+std = [481.243, 647.749]
+print(weight.size)
 std = st.t.interval(
-    alpha=0.95, df=weight.size-1,
+    alpha=0.05, df=weight.size-1,
     loc=weight.std(), scale=weight.sem()
 )
+
+
+def CHISQINV(q, df):
+    # https://stackoverflow.com/questions/53019080/chi2inv-in-python
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.chi2.html
+    return chi2.ppf(q, df)
+
+
+N = weight.size
+SD = weight.std()
+alpha = 0.05
+
+# https://www.graphpad.com/support/faq/the-confidence-interval-of-a-standard-deviation/
+lowerLimit = SD*math.sqrt((N-1)/CHISQINV(1-(alpha/2), N-1))
+upperLimit = SD*math.sqrt((N-1)/CHISQINV((alpha/2), N-1))
+
+# sigma σ - statndardhälve
 print("   sigma = %.3f [%.3f, %.3f]" %
 
-      (weight.std(), std[0], std[1])
+      (weight.std(), lowerLimit, upperLimit)
       )
 
 print('')